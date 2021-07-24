1997bd4ffe2ff4f172706514460c2af28851b4e7
huangy <597505910@qq.com>
2021-07-24 23:15:44 +0800

    update

--- zxtest/picture.py
+++ zxtest/picture.py
@@ -12,4 +12,5 @@ if __name__ == '__main__':
     # 选股图
     draw.draw_table(ball.pickSymbols(), sysUtil.today() + "_test")
 
+    # 龙虎榜图
     draw.draw_table(ball.longHuBang('2021-07-23')[1], sysUtil.today() + "_龙虎榜")
