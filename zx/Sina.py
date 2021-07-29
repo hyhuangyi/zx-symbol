e58cabebc8e4534dcae97a14bb293d2b1bc4f03b
huangy <597505910@qq.com>
2021-07-29 19:34:32 +0800

    update

--- zx/Sina.py
+++ zx/Sina.py
@@ -7,7 +7,7 @@ from prettytable import PrettyTable
 
 def getIndustryInfo():
     table = PrettyTable(
-        ['板块', '公司家数', '平均价格', '涨跌额', '涨跌幅', '总成交量', '总成交金额', '领涨股代码', '领涨股涨跌幅', '当前价', '领涨股涨跌额', '领涨股名称'])
+        ['板块', '公司家数', '平均价格', '涨跌幅', '领涨股代码', '领涨股名称', '涨幅', '当前价'])
     res = requests.get(const.sina_bk)
     res.encoding = 'gbk'
     table_text = re.findall('\{.*\}', res.text)[0]
@@ -16,8 +16,8 @@ def getIndustryInfo():
     for l in list_values:
         arr = l.split(",")
         table.add_row(
-            [arr[1], arr[2], round(float(arr[3]), 2), round(float(arr[4]), 2), round(float(arr[5]), 2),
-             str(round(float(arr[6]) / 10000, 2)) + " 万", str(round(float(arr[7]) / 100000000, 2)) + ' 亿', arr[8],
-             round(float(arr[9]), 2), round(float(arr[10]), 2), round(float(arr[11]), 2), arr[12]])
+            [arr[1], arr[2], round(float(arr[3]), 2), round(float(arr[5]), 2),
+             arr[8], arr[12],
+             str(round(float(arr[9]), 2)) + ' %', round(float(arr[10]), 2), ])
     res = table.get_string(sortby="涨跌幅", reversesort=True)
     return res
