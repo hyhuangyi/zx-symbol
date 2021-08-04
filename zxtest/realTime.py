7da82ee3939c78e232aba918a516864486edb5c4
huangy <597505910@qq.com>
2021-08-04 10:58:59 +0800

    update

--- zxtest/realTime.py
+++ zxtest/realTime.py
@@ -4,7 +4,7 @@ import util.SysUtil as sysUtil
 import util.DingdingNotifyUtil as notifyUtil
 
 codes = '000858,600779,002617'
-codes = '000969,002597'
+codes = '000969,002597,600798'
 time_sleep = 5
 
 if __name__ == '__main__':
