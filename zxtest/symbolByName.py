9780ab486984ff484ed5361ac0f1a50bbb7705df
huangy <597505910@qq.com>
2021-08-10 15:00:01 +0800

    update

--- zxtest/symbolByName.py
+++ zxtest/symbolByName.py
@@ -1,20 +1,28 @@
 import zx.Snowball as ball
 import zx.SymbolStore as store
 
-name = '安泰科技'
+
+# 打印评论
+def printComment(symbol):
+    # 评论
+    for i in range(10):
+        data = ball.symbolComment(symbol=symbol, page=i + 1)
+        for d in data:
+            print(d)
+
+
+name = '荃银高科'
+
 if __name__ == '__main__':
     symbol = store.get_symbol_by_name(name)
     # 实时信息
     print(ball.realTimeData(symbol)[1])
 
     # 主动资金信息
-    print(ball.capitalFlow(symbol)[1])
+    print(ball.capitalFlow(symbol, count=240)[1])
 
     # 盘口信息
     print(ball.panKou(symbol)[1])
 
     # 评论
-    for i in range(10):
-        data = ball.symbolComment(symbol=symbol, page=i + 1)
-        for d in data:
-            print(d)
+    printComment(symbol)
